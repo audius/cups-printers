1c9852b4cb0a86063e9e100ea4d789d57501d299
Fabian Affolter <mail@fabian-affolter.ch>
2020-06-09 12:29:18 +0200

    Prepare for 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.0 - 20200609
+
+- Clean-up
+
 ## 0.1.2 - 20181205
 
 - Update support for printer state
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
 
 if sys.argv[-1] == 'publish':
     os.system('python3 setup.py sdist upload')
@@ -15,7 +15,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='cups_printers',
-    version='0.1.1',
+    version='0.2.0',
     description='Get the printers from a running CUPS instance.',
     long_description=long_description,
     url='https://github.com/audiusGmbH/cups_printers',
