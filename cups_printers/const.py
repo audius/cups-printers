1f4af73a4a6c90b4111be4727a26137bdf604d14
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-09 23:06:36 +0100

    Update

--- cups_printers/const.py
+++ cups_printers/const.py
@@ -1,9 +1,9 @@
 """Constants used by CUPS printers."""
 PACKAGE_NAME = 'cups_printers'
 
-__version__ = '0.1.0'
+__version__ = '0.3.0'
 
-REQUIRED_PYTHON_VER = (3, 5, 3)
+REQUIRED_PYTHON_VER = (3, 8, 0)
 
 DEFAULT_SERVER = 'localhost1:631'
 DEFAULT_TIMEOUT = 5
