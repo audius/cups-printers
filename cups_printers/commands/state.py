f03b550c04adfa4dfc305bcdd76942144bc00b09
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-20 20:53:20 +0100

    Add support for output as table

--- cups_printers/commands/state.py
+++ cups_printers/commands/state.py
@@ -1,5 +1,7 @@
 """State plugin for the CUPS printers CLI."""
 import typer
+from rich.console import Console
+from rich.table import Table
 
 app = typer.Typer()
 
@@ -13,3 +15,26 @@ def raw(ctx: typer.Context):
 
     for printer in printers:
         typer.echo(f"{printers[printer]['printer-info']}, {printers[printer]['printer-state']}")
+
+
+@app.command()
+def table(ctx: typer.Context):
+    """Display current state of all printers as table."""
+    connection = ctx.obj.get("connection")
+
+    printers = connection.getPrinters()
+
+    table = Table(title="Printer states")
+
+    table.add_column("Printer", justify="left", style="cyan", no_wrap=True)
+    table.add_column("State")
+
+    for printer in printers:
+        details = printers[printer]
+        table.add_row(
+            printer,
+            str(details["printer-state"]),
+        )
+
+    console = Console()
+    console.print(table)
