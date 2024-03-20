f151c742e99e562dc8a3b1b3881165bc7a901c66
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-20 20:45:15 +0100

    Rename file and migrate to typer

---
+++ cups_printers/main.py
@@ -0,0 +1,54 @@
+"""Main part of cups-pinters."""
+import typer
+from typing_extensions import Annotated
+from validators import url
+import cups
+from cups_printers.commands import output, state
+from cups_printers.constants import DEFAULT_SERVER, DEFAULT_TIMEOUT
+
+app = typer.Typer()
+
+app.add_typer(output.app, name="output")
+#app.add_typer(queue.app, name="queue")
+app.add_typer(state.app, name="state")
+
+
+def validate_host(value: str):
+    """Check if host is a valid URL."""
+    if url(value):
+        raise typer.BadParameter("URL is not valid")
+    return value
+
+
+@app.callback()
+def main(
+    ctx: typer.Context,
+    server: Annotated[
+        str,
+        typer.Option(
+            envvar="CUPS_SERVER",
+            help="The server URL of CUPS instance",
+            prompt=True,
+            callback=validate_host,
+        ),
+    ] = DEFAULT_SERVER,
+    timeout: Annotated[
+        str,
+        typer.Option(
+            envvar="TIMEOUT",
+            help="Timeout for network operations",
+            prompt=True,
+        ),
+    ] = DEFAULT_TIMEOUT,
+):
+
+    try:
+        cups.setServer(server)
+        ctx.obj = {"connection": cups.Connection()}
+    except RuntimeError:
+        typer.echo("Server not available or unable to connect")
+        raise SystemExit
+
+
+if __name__ == "__main__":
+    app()
